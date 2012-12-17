344fd57801aa0523271ece4ab251252d212bc218
salah.ghanim <salah.ghanim@gmail.com>
2012-12-17 21:31:20 +0200

    tmp commit before adding logs

---
+++ age/ps/age/util/Logger.java
@@ -0,0 +1,12 @@
+package ps.age.util;
+
+public abstract class Logger {
+
+	public abstract void e(String tag, String msg);
+	public abstract void i(String tag, String msg);
+	public abstract void w(String tag, String msg);
+	public abstract void v(String tag, String msg);
+	public abstract void d(String tag, String msg);
+
+	
+}
