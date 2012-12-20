2b2e48b2e1b7f3f9c9bb44534614094496befd77
salah.ghanim <salah.ghanim@gmail.com>
2012-12-20 22:40:24 +0200

    first bluetooth commit

--- src/ps/age/hbb/RecordActivity.java
+++ src/ps/age/hbb/RecordActivity.java
@@ -14,8 +14,11 @@ import ps.age.hbb.core.DBWraper;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.AudioManager;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
@@ -35,14 +38,15 @@ public class RecordActivity extends Activity implements
 		MediaRecordEventListener {
 	public static final String tag = RecordActivity.class.getSimpleName();
 
-	private Handler mHandler;
+	private AudioManager  audioManager;
+	private Handler       mHandler;
 	private MediaRecorder mRecorder;
 	private MediaErrorBroadcastReceiver mReceiver;
-	private Button mRecord;
-	private ImageView mBack;
-	private TextView systemTime;
-	private TextView elapsedTime;
-	private TextView remainingTime;
+	private Button     mRecord;
+	private ImageView  mBack;
+	private TextView   systemTime;
+	private TextView   elapsedTime;
+	private TextView   remainingTime;
 	private RecordItem mItem;
 	private DateFormat fmt = DateFormat.getDateTimeInstance();
 	private SimpleDateFormat timerFormat = new SimpleDateFormat("HH:mm:ss");
@@ -50,6 +54,7 @@ public class RecordActivity extends Activity implements
 	private String tmpPath;
 	private long startTime;
 	private boolean isRecording;
+	private boolean useBluetooth;
 	private static final long updateSleep = 100;
 	private static final String OUT_PATH = Environment
 			.getExternalStorageDirectory().getAbsolutePath() + "/HBB/";
@@ -68,11 +73,16 @@ public class RecordActivity extends Activity implements
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.record);
 		Log.e(tag, "onCreate");
-		mRecord = (Button) findViewById(R.id.recordButton);
-		mBack = (ImageView) findViewById(R.id.back);
-		systemTime = (TextView) findViewById(R.id.systemTime_Value);
-		elapsedTime = (TextView) findViewById(R.id.elapsedTime_Value);
-		remainingTime = (TextView) findViewById(R.id.remainingTime_Value);
+		
+		audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+		registerReceiver(bluetoothReceiver, new IntentFilter(AudioManager.ACTION_SCO_AUDIO_STATE_CHANGED));
+		audioManager.startBluetoothSco();
+		
+		mRecord 	  = (Button)    findViewById(R.id.recordButton);
+		mBack		  = (ImageView) findViewById(R.id.back);
+		systemTime	  = (TextView)  findViewById(R.id.systemTime_Value);
+		elapsedTime	  = (TextView)  findViewById(R.id.elapsedTime_Value);
+		remainingTime = (TextView)  findViewById(R.id.remainingTime_Value);
 
 		mRecord.setOnClickListener(listener);
 		mBack.setOnClickListener(new OnClickListener() {
@@ -154,7 +164,9 @@ public class RecordActivity extends Activity implements
 				stopRecording();
 				new File(tmpPath).delete();
 			}
-			
+			audioManager.stopBluetoothSco();
+			if(!useBluetooth)
+                unregisterReceiver(bluetoothReceiver);
 			mHandler.removeCallbacks(timeRunnable);
 		}
 	}
@@ -192,6 +204,11 @@ public class RecordActivity extends Activity implements
 		mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
 		mRecorder.setOutputFormat(MediaRecorder.VideoEncoder.DEFAULT);
 		mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+		if(useBluetooth){
+			//	for bluetooth devices
+			mRecorder.setAudioChannels(1);
+			mRecorder.setAudioSamplingRate(8000);
+		}
 		tmpPath = OUT_PATH + String.valueOf(System.currentTimeMillis())
 				+ ".3gp";
 
@@ -324,4 +341,27 @@ public class RecordActivity extends Activity implements
 		// TODO Auto-generated method stub
 
 	}
+	BroadcastReceiver bluetoothReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int state = intent.getIntExtra(AudioManager.EXTRA_SCO_AUDIO_STATE, -1);
+            Log.d(tag, "Audio SCO state: " + state);
+
+            if (AudioManager.SCO_AUDIO_STATE_CONNECTED == state) { 
+                /* 
+                 * Now the connection has been established to the bluetooth device. 
+                 * Record audio or whatever (on another thread).With AudioRecord you can record with an object created like this:
+                 * new AudioRecord(MediaRecorder.AudioSource.MIC, 8000, AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                 * AudioFormat.ENCODING_PCM_16BIT, audioBufferSize);
+                 *
+                 * After finishing, don't forget to unregister this receiver and
+                 * to stop the bluetooth connection with am.stopBluetoothSco();
+                 */
+            	useBluetooth = true;
+                unregisterReceiver(this);
+            }
+
+        }
+    };
 }
